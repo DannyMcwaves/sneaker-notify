78eef9484851ee6b4031431a56a8e23713df1fa3
dannymcwaves <dannymcwaves96@gmail.com>
2018-12-26 14:16:21 +0000

    config setup[

---
+++ notify/config.py
@@ -0,0 +1,11 @@
+# this has constants that are needed throughout the app for connections to configurations.
+
+REDIS_URL = '127.0.0.1'
+PORT = '6387'
+SLACK_URL = 'https://hooks.slack.com/services/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'
+KITH_CONTAINER = []
+NIKE_CONTAINER = []
+YEEZY_CONTAINER = []
+ADIDAS_CONTAINER = []
+PROXY = open('./proxies.txt', 'r').readlines()
+APIKEY = "587f112b079640f18981a0bd33eb87a5"
