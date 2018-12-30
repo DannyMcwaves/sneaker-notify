6701626225f6d41d798b4c856d3f012f4f5579b7
dannymcwaves <dannymcwaves96@gmail.com>
2018-12-30 16:12:32 +0000

    Update cache utilities and functionalities for inserting, selecting data into created tables

--- notify/util.py
+++ notify/util.py
@@ -1,17 +1,64 @@
 
-__all__ = ['check_availability', 'check_availability_yeezy']
+__all__ = ['select_data', 'create_tables', 'insert_data']
+from .db_util import db_connect
+from config import SQLITE_DB
 
 
-def check_availability(container, item):
-	common_name = [x['name'] for x in container]
-	common_colors = [x['colors'] for x in container]
-
-	data = item if item['name'] not in common_name and item['colors'] not in common_colors else []
-	return data
+# Instantiate the database.
+con = db_connect(SQLITE_DB)
+cursor = con.cursor()
 
 
-def check_availability_yeezy(container, item):
-	common_name = [x['name'] for x in container]
+# Database SQL Statements.
+sql_format_create = """
+CREATE TABLE IF NOT EXISTS {} (
+	name text NOT NULL,
+	price text NOT NULL,
+	image text NOT NULL UNIQUE,
+	date DATE NOT NULL,
+	link text NOT NULL UNIQUE
+)
+"""
 
-	data = item if item['name'] not in common_name else []
-	return data
+sql_format_insert = """
+INSERT INTO {table} (name, price, image, link, date) VALUES (\"{name}\", \"{price}\", \"{image}\", \"{link}\", \"{date}\")
+"""
+
+sql_format_select = """
+SELECT name, price, image, link, date FROM {}
+"""
+
+kith_sql = sql_format_create.format('kith')
+nike_sql = sql_format_create.format('nike')
+yeezy_sql = sql_format_create.format('yeezy')
+adidas_sql = sql_format_create.format('adidas')
+
+
+def create_tables():
+	try:
+		# execute the creation of this table
+		for i in [kith_sql, nike_sql, yeezy_sql, adidas_sql]:
+			cursor.execute(i)
+		return cursor
+	except Exception as err:
+		print(err)
+
+
+def insert_data(table, name, price, image, link, date):
+	try:
+		sql = sql_format_insert.format(table=table, name=name, price=price, image=image, link=link, date=date)
+		cursor.execute(sql)
+		con.commit()
+		return 'Done'
+	except Exception as err:
+		print(err)
+		return None
+
+
+def select_data(table):
+	try:
+		cursor.execute(sql_format_select.format(table))
+		return cursor.fetchall()
+	except Exception as err:
+		print('this error is from selecting')
+		print(err)
