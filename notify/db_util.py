c48d86306b29d029fe2caa98a29e25a557a2876b
dannymcwaves <dannymcwaves96@gmail.com>
2019-01-06 20:45:43 +0000

    redis class based refactor for SQLite and redis

--- notify/db_util.py
+++ notify/db_util.py
@@ -1,6 +1,7 @@
 # db_utils.py
 import os
 import sqlite3
+import redis
 
 # create a default path to connect to and create (if necessary) a database
 # called 'database.sqlite3' in the same directory as this script
@@ -11,3 +12,10 @@ def db_connect(db_path=DEFAULT_PATH):
     con = sqlite3.connect(db_path)
     return con
 
+
+def redis_connect():
+    """
+    this connection based protocol is for redis and is to be used for key based data storage.
+    :return:
+    """
+    return redis.Redis(host='localhost', port=6379, db=0)
