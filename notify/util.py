d5c32a47bbaf00d4601516f9cc9d2fedf8052b55
dannymcwaves <dannymcwaves96@gmail.com>
2019-01-06 20:43:20 +0000

    Redis data persistence for caching provisioned

--- notify/util.py
+++ notify/util.py
@@ -1,67 +1,78 @@
 
-__all__ = ['select_data', 'create_tables', 'insert_data']
-from .db_util import db_connect
+__all__ = ['SQLite']
+from .db_util import db_connect, redis_connect
 from config import SQLITE_DB
 
 
-# Instantiate the database.
-con = db_connect(SQLITE_DB)
-cursor = con.cursor()
+class SQLite:
+
+	# get and initialize the database connection with config DB.
+	con = db_connect(SQLITE_DB)
+
+	# get the cursor for writing and reading to the database.
+	cursor = con.cursor()
+
+	# Database SQL Statements.
+	sql_format_create = """
+	CREATE TABLE IF NOT EXISTS {} (
+		name text NOT NULL,
+		price text NOT NULL,
+		image text NOT NULL UNIQUE,
+		date DATE NOT NULL,
+		link text NOT NULL UNIQUE
+	)
+	"""
+
+	sql_format_insert = """
+	INSERT INTO {table} (name, price, image, link, date) VALUES (\"{name}\", \"{price}\", \"{image}\", \"{link}\", \"{date}\")
+	"""
+
+	sql_format_select = """
+	SELECT name, price, image, link, date FROM {}
+	"""
+
+	dbs = ['kith', 'nike', 'yeezy', 'adidas', 'rise', 'addict', 'nicekicks']
+
+	def create_tables(self):
+		try:
+			# execute the creation of this table
+			for i in self.dbs:
+				self.cursor.execute(self.sql_format_create.format(i))
+			return self.cursor
+		except Exception as err:
+			print(err)
+
+	def insert_data(self, table, name, price, image, link, date):
+		try:
+			sql = self.sql_format_insert.format(table=table, name=name, price=price, image=image, link=link, date=date)
+			self.cursor.execute(sql)
+			self.con.commit()
+			return 'Done'
+		except Exception as err:
+			print(err.with_traceback(None))
+			return None
+
+	def select_data(self, table):
+		try:
+			self.cursor.execute(self.sql_format_select.format(table))
+			return self.cursor.fetchall()
+		except Exception as err:
+			print('this error is from selecting')
+			print(err)
 
 
-# Database SQL Statements.
-sql_format_create = """
-CREATE TABLE IF NOT EXISTS {} (
-	name text NOT NULL,
-	price text NOT NULL,
-	image text NOT NULL UNIQUE,
-	date DATE NOT NULL,
-	link text NOT NULL UNIQUE
-)
-"""
+class Redis:
 
-sql_format_insert = """
-INSERT INTO {table} (name, price, image, link, date) VALUES (\"{name}\", \"{price}\", \"{image}\", \"{link}\", \"{date}\")
-"""
+	redis = redis_connect()
 
-sql_format_select = """
-SELECT name, price, image, link, date FROM {}
-"""
+	def push(self, prop, val):
+		return self.redis.lpush(prop, val)
 
-kith_sql = sql_format_create.format('kith')
-nike_sql = sql_format_create.format('nike')
-yeezy_sql = sql_format_create.format('yeezy')
-adidas_sql = sql_format_create.format('adidas')
-rise_sql = sql_format_create.format('rise')
-addict_sql = sql_format_create.format('addict')
-nice_kicks_sql = sql_format_create.format('nicekicks')
+	def pull(self, prop):
+		return self.redis.lrange(prop, 0, -1)
 
+	def add(self, prop, data):
+		return self.redis.sadd(prop, "{} {}".format(data['link'], data['image']))
 
-def create_tables():
-	try:
-		# execute the creation of this table
-		for i in [kith_sql, nike_sql, yeezy_sql, adidas_sql, rise_sql, addict_sql, nice_kicks_sql]:
-			cursor.execute(i)
-		return cursor
-	except Exception as err:
-		print(err)
-
-
-def insert_data(table, name, price, image, link, date):
-	try:
-		sql = sql_format_insert.format(table=table, name=name, price=price, image=image, link=link, date=date)
-		cursor.execute(sql)
-		con.commit()
-		return 'Done'
-	except Exception as err:
-		print(err.with_traceback(None))
-		return None
-
-
-def select_data(table):
-	try:
-		cursor.execute(sql_format_select.format(table))
-		return cursor.fetchall()
-	except Exception as err:
-		print('this error is from selecting')
-		print(err)
+	def __call__(self, *args, **kwargs):
+		return self.redis
