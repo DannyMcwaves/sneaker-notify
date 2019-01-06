1ac796a6879459e58c2322da9bf1b9ad5a97cd53
dannymcwaves <dannymcwaves96@gmail.com>
2019-01-06 20:42:52 +0000

    add tests for the redis cacche engine

--- db_test.py
+++ db_test.py
@@ -1,34 +1,9 @@
 
-from notify.util import select_data, insert_data, create_tables
-import random
+from notify.util import SQLite, Redis
 
 
-data = [
-{'colors': '',
- 'date': '2018-12-30',
- 'image': 'https://cdn.shopify.com/s/files/1/1765/5971/products/fin_440x.jpg?v=1543693873',
- 'link': 'https://yeezysupply.com/products/mens-combat-boot-in-thick-suede-military?c=%2Fcollections%2Fnew-arrivals-footwear%2F',
- 'name': 'THICK SUEDE COMBAT BOOT',
- 'price': '$220'},
-{'colors': '',
- 'date': '2018-12-30',
- 'image': 'https://cdn.shopify.com/s/files/1/1765/5971/products/la_440x.jpg?v=1543693861',
- 'link': 'https://yeezysupply.com/products/mens-combat-boot-in-thick-suede-graphite?c=%2Fcollections%2Fnew-arrivals-footwear%2F',
- 'name': 'THICK SUEDE COMBAT BOOT',
- 'price': '$220'},
-{'colors': '',
- 'date': '2018-12-30',
- 'image': 'https://cdn.shopify.com/s/files/1/1765/5971/products/KW2581.022_Side1_color_onyx-shade_10313325-1773-49dd-9aea-6b9861925324_440x.jpg?v=1544040245',
- 'link': 'https://yeezysupply.com/products/womens-military?c=%2Fcollections%2Fnew-arrivals-footwear%2F',
- 'name': 'WOMENS BOOT',
- 'price': '$220'},
-{'colors': '',
- 'date': '2018-12-30',
- 'image': 'https://cdn.shopify.com/s/files/1/1765/5971/products/KM2606.012_Side1_color_onyx-shade_70012e4e-b24a-401a-992f-81e61300586f_440x.jpg?v=1544039929',
- 'link': 'https://yeezysupply.com/products/mens-military-boot?c=%2Fcollections%2Fnew-arrivals-footwear%2F',
- 'name': 'MENS BOOT',
- 'price': '$220'}
-]
+sqlite = SQLite()
+redis = Redis()
 
 db = {
  'colors': 'White / Green / Blue',
@@ -41,12 +16,12 @@ db = {
  'sizes': ['13', '']
 }
 
-cursor = create_tables()
+cursor = sqlite.create_tables()
 
-d = random.choice(data)
-
-res = insert_data('kith', name=str(db['name']), price=db['price'], image=db['image'], link=db['link'], date=db['date'])
-print(res)
-
-da = select_data('yeezy')
-print(len(da))
+# redis.push('nike', 'Airmax')
+# redis.push('nike', 'Airmax Jordan')
+# redis.push('nike', 'Jordan Kicks')
+val = redis().sadd('kith', "{} {}".format(db['link'], db['image']))
+print(val)
+# print(redis().linsert('nike', 'after', 'Airmax Jordan', "Airmax Kobe"))
+print(redis.pull('nike'))
