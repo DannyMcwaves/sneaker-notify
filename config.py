8452f539be79195bb76ed2d98f027851eb2e7c87
dannymcwaves <dannymcwaves96@gmail.com>
2019-01-07 11:03:17 +0000

    add redis details

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 # this has constants that are needed throughout the app for connections to configurations.
 
-REDIS_URL = '127.0.0.1'
 PORT = '6387'
 SLACK_URL = 'https://hooks.slack.com/services/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'
 KITH_CONTAINER = []
@@ -11,3 +10,7 @@ PROXY = open('./proxies.txt', 'r').readlines()
 APIKEY = "587f112b079640f18981a0bd33eb87a5"
 # SQLITE_DB = './database.db'
 SQLITE_DB = './database_prod.db'
+REDIS_URL = 'ec2-18-215-97-111.compute-1.amazonaws.com'
+REDIS_PORT = 32099
+REDIS_PASSWORD = 'pf21ffb61b29308455434259ae2ebeaceddbcda3c384fc17e3226e80f6608cf14'
+REDIS_USER = 'h'
