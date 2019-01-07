df8d3703c0ab6e896c976940cefbf7256683f378
dannymcwaves <dannymcwaves96@gmail.com>
2019-01-07 11:03:48 +0000

    redis database url and pass for heroku added

--- notify/db_util.py
+++ notify/db_util.py
@@ -1,11 +1,16 @@
 # db_utils.py
+from config import REDIS_PASSWORD, REDIS_PORT, REDIS_URL, REDIS_USER
 import os
 import sqlite3
 import redis
 
+
 # create a default path to connect to and create (if necessary) a database
 # called 'database.sqlite3' in the same directory as this script
 DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'database.sqlite3')
+# con = redis.Redis(host=REDIS_URL, password=REDIS_PASSWORD, port=REDIS_PORT)
+
+# print(con)
 
 
 def db_connect(db_path=DEFAULT_PATH):
@@ -18,4 +23,4 @@ def redis_connect():
     this connection based protocol is for redis and is to be used for key based data storage.
     :return:
     """
-    return redis.Redis(host='localhost', port=6379, db=0)
+    return redis.Redis(host=REDIS_URL, password=REDIS_PASSWORD, port=REDIS_PORT)
